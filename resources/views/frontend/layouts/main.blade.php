4125a974e80390c0d68535f5ed097934b5170e7a
unknown <ikangoreng196@gmail.com>
2025-12-17 21:47:59 +0700

    Update hero background dan overlay

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -153,7 +153,7 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-6">
-                                <script>document.write(new Date().getFullYear())</script> © PT.Nadi Nusantara Teknologi.
+                                <script>document.write(new Date().getFullYear())</script> © Nadiwarga.
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
